5dd1bae4863cd2603dc4b177b6ac093c16c6322f
zeyti12a <daviraposo124@gmail.com>
2023-11-23 15:13:30 -0300

    a

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express() 
+
+app.engine('handlebars' , exphbs.engine())
+app.set('view engine' , "handlebars")
+
+app.use(express.static("public"))
+
+app.get ('/', (requisicao, resposta)=>{
+   resposta.render('home') 
+}) 
+
+app.listen(3000, () => {
+    console.log ("Servidor Rodando")
+})
